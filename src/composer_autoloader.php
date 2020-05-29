910b700703e9c32531bddf96d1c467d1bf8c59ba
Nguyễn Hải Quang <haiquang9994@gmail.com>
2020-05-29 15:21:29 +0700

    fix require

---
+++ src/composer_autoloader.php
@@ -0,0 +1,17 @@
+<?php
+
+return function () {
+    $files = [
+        __DIR__ . '/../../../autoload.php',
+        __DIR__ . '/../vendor/autoload.php',
+    ];
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            require_once $file;
+
+            return true;
+        }
+    }
+
+    return false;
+};
